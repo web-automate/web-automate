8d6d5145138f997500f9ac6fe067e0ac8028f969
misbakhul29 <misbakhul2904@gmail.com>
2026-01-29 20:33:19 +0700

    fix: seeder

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,6 +1,9 @@
 import { PrismaPg } from "@prisma/adapter-pg";
+import dotenv from 'dotenv';
 import { Pool } from "pg";
 import { PrismaClient } from "./generated/prisma/client";
+
+dotenv.config();
 
 const connectionString = process.env.DATABASE_URL;
 
--- packages/database/prisma/seed.ts
+++ packages/database/prisma/seed.ts
@@ -1,40 +1,38 @@
-import * as bcrypt from "bcryptjs";
-import { Role } from "../generated/prisma/enums";
-import { prisma } from "../index";
+import { prisma } from "..";
 
 async function main() {
   console.log('🌱 Starting seeding...');
 
-  const email = 'admin@ha.com';
-  const password = 'admin123';
+  // const email = 'admin@ha.com';
+  // const password = 'admin123';
 
-  await prisma.account.deleteMany({ where: { accountId: email } });
+  // await prisma.account.deleteMany({ where: { accountId: email } });
 
-  const owner = await prisma.user.upsert({
-    where: { email },
-    update: {},
-    create: {
-      name: 'Admin Web',
-      email,
-      emailVerified: true,
-      role: Role.ADMIN,
-    },
-  });
+  // const owner = await prisma.user.upsert({
+  //   where: { email },
+  //   update: {},
+  //   create: {
+  //     name: 'Admin Web',
+  //     email,
+  //     emailVerified: true,
+  //     role: "ADMIN",
+  //   },
+  // });
 
-  const hashedPassword = await bcrypt.hash(password, 10);
+  // const hashedPassword = await bcrypt.hash(password, 10);
 
-  await prisma.account.create({
-    data: {
-      userId: owner.id,
-      accountId: email, 
-      providerId: 'email',
-      password: hashedPassword,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-  });
+  // await prisma.account.create({
+  //   data: {
+  //     userId: owner.id,
+  //     accountId: email, 
+  //     providerId: 'email',
+  //     password: hashedPassword,
+  //     createdAt: new Date(),
+  //     updatedAt: new Date(),
+  //   },
+  // });
 
-  console.log('✅ Owner and Account credentials created');
+  // console.log('✅ Owner and Account credentials created');
 
   const templateMinimal = await prisma.template.upsert({
     where: { slug: 'minimalist-blog' },
@@ -52,36 +50,36 @@ async function main() {
     },
   });
 
-  const website = await prisma.website.upsert({
-    where: { domain: 'demo.web.id' },
-    update: {},
-    create: {
-      name: 'Web Demo',
-      domain: 'demo.web.id',
-      templateId: templateMinimal.id,
-      ownerId: owner.id,
-      status: 'PUBLISHED',
-    },
-  });
+  // const website = await prisma.website.upsert({
+  //   where: { domain: 'demo.web.id' },
+  //   update: {},
+  //   create: {
+  //     name: 'Web Demo',
+  //     domain: 'demo.web.id',
+  //     templateId: templateMinimal.id,
+  //     ownerId: owner.id,
+  //     status: 'PUBLISHED',
+  //   },
+  // });
 
-  console.log('✅ Website created');
+  // console.log('✅ Website created');
 
-  await prisma.author.upsert({
-    where: { slug: 'jax-ai' },
-    update: {
-      websites: { connect: { id: website.id } }
-    },
-    create: {
-      name: 'Jax Intelligence',
-      slug: 'jax-ai',
-      image: 'https://api.dicebear.com/7.x/bottts/svg?seed=Jax',
-      bio: 'AI-driven content creator specializing in digital trend analysis and modern lifestyle.',
-      websiteUrl: 'https://web.id',
-      websites: { connect: { id: website.id } }
-    },
-  });
+  // await prisma.author.upsert({
+  //   where: { slug: 'jax-ai' },
+  //   update: {
+  //     websites: { connect: { id: website.id } }
+  //   },
+  //   create: {
+  //     name: 'Jax Intelligence',
+  //     slug: 'jax-ai',
+  //     image: 'https://api.dicebear.com/7.x/bottts/svg?seed=Jax',
+  //     bio: 'AI-driven content creator specializing in digital trend analysis and modern lifestyle.',
+  //     websiteUrl: 'https://web.id',
+  //     websites: { connect: { id: website.id } }
+  //   },
+  // });
 
-  console.log('✅ Author created and linked to website');
+  // console.log('✅ Author created and linked to website');
   console.log('🏁 Seeding finished.');
 }
 
