1e9d1bc9ab33a4cce3d8d01b91720a91873d4877
misbakhul29 <misbakhul2904@gmail.com>
2026-02-13 18:03:23 +0700

    revert(client): base url better auth client

--- apps/client/lib/auth.client.ts
+++ apps/client/lib/auth.client.ts
@@ -1,6 +1,8 @@
 import { createAuthClient } from "better-auth/client";
 
-export const authClient = createAuthClient();
+export const authClient = createAuthClient({
+    baseURL: process.env.NEXT_PUBLIC_APP_URL,
+});
 
 export const { 
     signIn, 
