4727914c513d48a9f3293df23a2b029eea940e51
misbakhul29 <misbakhul2904@gmail.com>
2026-02-13 17:46:43 +0700

    refactor(client): removing base url better auth client

--- apps/client/lib/auth.client.ts
+++ apps/client/lib/auth.client.ts
@@ -1,10 +1,6 @@
 import { createAuthClient } from "better-auth/client";
 
-const appUrl = process.env.NEXT_PUBLIC_APP_URL;
-
-export const authClient = createAuthClient({
-    baseURL: appUrl,
-});
+export const authClient = createAuthClient();
 
 export const { 
     signIn, 
